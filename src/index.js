351e0e867a2d39727b20199369c7301c878ec304
Thomas <tho.daskalakis@gmail.com>
2022-09-01 17:03:53 +0300

    Initialize project

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './styles.scss';
+
+// Get container
+
+const container = document.querySelector('.container');
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(s[ac]ss)$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
