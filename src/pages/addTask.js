4ff1f2f69f4e2a6d2e1d5214ceea439103cead3a
Thomas <tho.daskalakis@gmail.com>
2022-09-02 17:14:23 +0300

    Add task action button, make btns responsive

--- src/pages/addTask.js
+++ src/pages/addTask.js
@@ -30,6 +30,8 @@ function addTask(tasks) {
     '../src/svg/edit_square_FILL0_wght400_GRAD0_opsz48.svg'
   );
 
+  editIcon.setAttribute('alt', 'Edit task');
+
   const editBtn = document.createElement('button');
 
   editBtn.appendChild(editIcon);
@@ -43,6 +45,21 @@ function addTask(tasks) {
   const taskActions = document.createElement('div');
 
   taskActions.classList.add('task-action');
+
+  const actionsImg = document.createElement('img');
+
+  actionsImg.setAttribute(
+    'src',
+    '../src/svg/done_FILL0_wght400_GRAD0_opsz48.svg'
+  );
+
+  actionsImg.setAttribute('alt', 'Mark task as done');
+
+  const actionsBtn = document.createElement('button');
+
+  actionsBtn.appendChild(actionsImg);
+
+  taskActions.appendChild(actionsBtn);
 
   task.appendChild(taskActions);
 
