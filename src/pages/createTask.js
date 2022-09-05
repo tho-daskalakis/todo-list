6756d5d8d21dc77a8550dd0e29e33fa519ca3092
Thomas <tho.daskalakis@gmail.com>
2022-09-05 17:06:50 +0300

    Create task edit mode

--- src/pages/createTask.js
+++ src/pages/createTask.js
@@ -1,9 +1,12 @@
 class Task {
   constructor(tasks) {
-    this.taskNumber = tasks.length + 1;
+    this.taskIndex = tasks.length;
+    this.tasks = tasks;
   }
 
   _text = 'Task';
+
+  _div = document.createElement('div');
 
   set text(text) {
     if (!text) return;
@@ -13,6 +16,14 @@ class Task {
 
   get text() {
     return this._text;
+  }
+
+  set div(div) {
+    this._div = div;
+  }
+
+  get div() {
+    return this._div;
   }
 
   get task() {
@@ -26,7 +37,7 @@ class Task {
 
     taskTitle.classList.add('task-title');
 
-    taskTitle.textContent = `${this._text} ${this.taskNumber}`;
+    taskTitle.textContent = `${this._text} ${this.taskIndex + 1}`;
 
     task.appendChild(taskTitle);
 
@@ -48,8 +59,12 @@ class Task {
     const editBtn = document.createElement('button');
 
     editBtn.addEventListener('click', (e) => {
-      console.log(this);
-      // Create edit mode
+      // console.log(this);
+
+      // Call edit mode
+
+      this.div;
+      this.editTask;
     });
 
     editBtn.appendChild(editIcon);
@@ -81,7 +96,80 @@ class Task {
 
     task.appendChild(taskActions);
 
-    return task;
+    this.div = task;
+
+    return this.div;
+  }
+
+  get editTask() {
+    // Delete elements
+    const taskComponents = [...this.div.childNodes];
+    console.log(taskComponents);
+
+    const title = taskComponents[0];
+    this.div.removeChild(title);
+
+    const editBtn = taskComponents[1];
+    this.div.removeChild(editBtn);
+
+    const doneBtn = taskComponents[2];
+    this.div.removeChild(doneBtn);
+
+    // Create new elements
+
+    // Create text
+
+    const editTitle = document.createElement('div');
+    editTitle.classList.add('task-title');
+
+    const input = document.createElement('input');
+    input.setAttribute('type', 'text');
+
+    editTitle.appendChild(input);
+    this.div.appendChild(editTitle);
+
+    // Create apply edit button
+
+    const applyChanges = document.createElement('div');
+    applyChanges.classList.add('task-edit');
+
+    const applyBtn = document.createElement('button');
+    applyBtn.addEventListener('click', (e) => {
+      console.log('Apply changes');
+    });
+
+    const applyImg = document.createElement('img');
+    applyImg.setAttribute(
+      'src',
+      '../src/svg/done_FILL0_wght400_GRAD0_opsz48.svg'
+    );
+
+    applyBtn.appendChild(applyImg);
+    applyChanges.appendChild(applyBtn);
+    this.div.appendChild(applyChanges);
+
+    // Create delete task button
+
+    const deleteAction = document.createElement('div');
+    deleteAction.classList.add('task-action');
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('task-delete');
+    deleteBtn.addEventListener('click', (e) => {
+      console.log('Delete task');
+    });
+
+    const deleteImg = document.createElement('img');
+    deleteImg.setAttribute(
+      'src',
+      '../src/svg/delete_FILL0_wght400_GRAD0_opsz48.svg'
+    );
+
+    deleteBtn.appendChild(deleteImg);
+    deleteAction.appendChild(deleteBtn);
+    this.div.appendChild(deleteAction);
+
+    console.log(input, editTitle);
   }
 }
 
