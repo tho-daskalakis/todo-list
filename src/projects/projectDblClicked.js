e0fe9bafa07d34b23a929ce9d8cd4824187bbb9f
Thomas <tho.daskalakis@gmail.com>
2022-10-10 15:10:53 +0300

    Implement projectDblClicked

--- src/projects/projectDblClicked.js
+++ src/projects/projectDblClicked.js
@@ -1,5 +1,11 @@
+import { displayManager } from '../view/displayManager';
+
 function projectDblClicked(project) {
-  console.log('dbl clicked', project.projectName);
+  // console.log('dbl clicked', project.projectName);
+
+  displayManager.toggleAddProjectBtn();
+  displayManager.showEditDialog(project);
+  displayManager.selectProjectInput();
 }
 
 export { projectDblClicked };
